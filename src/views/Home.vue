76ef99eab9f1b380292c168c5636fafb2eb167a7
Osh <gusmunos@student.gu.se>
2021-12-28 18:54:26 +0100

    done lesson 3 fade in out in toast

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,20 @@
 <template>
   <div class="home">
-    <Toast v-if="showToast" />
-    <Todos @badValue="triggerToast" />
-    <transition name="fade">
-      <div v-if="showP">hello, ninjas</div>
+    <transition name="toast">
+      <Toast v-if="showToast"/>
     </transition>
-    <button @click="showP = !showP">toggle</button>
+    <Todos @badValue="triggerToast"/>
+
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import {ref} from 'vue'
 import Toast from '../components/Toast'
 import Todos from '../components/Todos'
 
 export default {
-  components: { Toast, Todos },
+  components: {Toast, Todos},
   setup() {
     const showToast = ref(false)
     const showP = ref(false)
@@ -25,28 +24,39 @@ export default {
       setTimeout(() => showToast.value = false, 3000)
     }
 
-    return { showToast, triggerToast, showP }
+    return {showToast, triggerToast}
   }
 }
 </script>
 
 <style>
-  .fade-enter-from {
-    opacity: 0;
-  }
-  .fade-enter-to {
-    opacity: 1;
-  }
-  .fade-enter-active {
-    transition: all 2s ease;
-  }
-  .fade-leave-from {
-    opacity: 1;
-  }
-  .fade-leave-to {
-    opacity: 0;
-  }
-  .fade-leave-active {
-    transition: all 2s ease;
-  }
+.toast-enter-from {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+
+.toast-enter-to {
+  opacity: 1;
+  transform: translateY(0px);
+}
+
+.toast-enter-active {
+  transition: all 0.3s ease-in;
+}
+
+.toast-leave-from {
+  opacity: 1;
+  transform: translateY(0px);
+}
+
+.toast-leave-to {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+
+.toast-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+
 </style>
